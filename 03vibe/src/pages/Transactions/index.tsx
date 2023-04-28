ec62ebd1a4aefeed39bb9676f7b251066e72ebbf
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2023-04-28 20:45:13 -0300

    Formatação do preço

--- 03vibe/src/pages/Transactions/index.tsx
+++ 03vibe/src/pages/Transactions/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 interface Transaction {
@@ -41,11 +42,12 @@ export function Transactions() {
                   <td width="50%">{transaction.description}</td>
                   <td>
                     <PriceHighlight variant={transaction.type}>
-                      {transaction.price}
+                    {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                 </tr>
               )
             })}
